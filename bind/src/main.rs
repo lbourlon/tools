6c9c5b096ffb34420af3ca611216f3d174252da5
Léon Bourlon <leonbourlon@hotmail.com>
2025-02-23 16:30:01 +0100

    testing with ascii display

--- bind/src/main.rs
+++ bind/src/main.rs
@@ -1,6 +1,8 @@
+use core::fmt;
 use std::env;
 use std::fs::File;
 use std::io::{self, Read, Result};
+use std::str::from_utf8;
 
 struct BindConfig {
     cols: usize,
@@ -23,16 +25,27 @@ impl FormatAsHex for Vec<u8> {
         let mut hex_string = String::new();
 
         let max_in_one_line = bind_config.cols * bind_config.groups;
+        let max_numer_of_lines = self.len() / max_in_one_line;
+        let mut bytes_written_in_line = 0;
+
+        let mut line_num = 0;
+        hex_string.push_str(&format!("{:08x}: ", line_num * 0x10)); // address prepend
         for (i, byte) in self.iter().enumerate() {
             hex_string.push_str(&format!("{:02x}", byte));
+            bytes_written_in_line += 1;
 
-            if (i + 1) % max_in_one_line == 0 {
+            if (i + 1) % max_in_one_line == 0 && line_num  < max_numer_of_lines {
+                hex_string.push_str(&format!("  {}", from_utf8(&self[i+1-bytes_written_in_line..=i]).unwrap())); // ascii
                 hex_string.push_str(&'\n'.to_string());
+                bytes_written_in_line = 0;
+                line_num += 1;
+                hex_string.push_str(&format!("{:08x}: ", i + 1)); // address prepend
             }
             else if (i + 1) % bind_config.groups == 0 {
                 hex_string.push_str(&' '.to_string());
             }
         }
+        // hex_string.push_str(&format!(" {}", from_utf8(&self[self.len()-bytes_written_in_line..]).unwrap())); // ascii
         hex_string
     }
 }
@@ -54,6 +67,8 @@ fn help() {
     println!("  -c, --cols    number of cols. [Default 8]");
     println!("  -g, --group   number of octets per group. [Default 2]");
     println!("  -f, --file    file to read from ('-' for stdin)");
+    println!("  -l, --len     stop after <len> octets");
+    println!("  -a            prepend byte");
 }
 
 
@@ -95,7 +110,6 @@ fn parse_args() ->  Result<BindConfig>  {
             }
         }
     }
-
     Ok(hex_dump_config)
 }
 
