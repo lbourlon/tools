089d0f9fd9cf04d5aa49621ead40bf81405ca2a7
Léon Bourlon <leonbourlon@hotmail.com>
2025-02-24 23:46:56 +0100

    add raw hex options

--- bind/readme.md
+++ bind/readme.md
@@ -1,3 +1,16 @@
 # Bind
 
 It's a binary dumper, think xdd, but with less features.
+
+```
+Usage : <filename> or '-' for reading from stdin
+
+Options :
+  -c, --cols               number of cols                      [Default 8]
+  -g, --group              number of octets per group          [Default 2]
+  -f, --file  <file_path>  file to read from ('-' for stdin)
+  -r, --raw   <raw_style>  choose between hex, esc and C.
+      hex : straight hex values ie 'cafe'
+      esc : escaped hex values ie '\xca\xfe'
+      C   : C include style ie '0xca, 0xfe'
+```
--- bind/src/main.rs
+++ bind/src/main.rs
@@ -1,26 +1,45 @@
 use std::env;
 use std::fs::File;
 use std::io::{self, Read, Result};
-// use std::str::from_utf8;
 use std::process;
 
 struct BindConfig {
     cols: usize,
     groups: usize,
     file_name: String,
+    raw: bool,
+    raw_mode: String,
 }
 
 trait FormatAsHex {
     #[allow(dead_code)]
-    fn format_as_hex(&self) -> String;
+    fn format_as_raw_hex(&self, bind_config: BindConfig) -> String;
     fn format_as_hex_pretty_print(&self, bind_config: BindConfig) -> String;
 }
 
 impl FormatAsHex for Vec<u8> {
-    fn format_as_hex(&self) -> String {
-        self.iter().map(|byte| format!("{:02x}", byte)).collect()
+    fn format_as_raw_hex(&self, bind_config: BindConfig) -> String {
+        match &bind_config.raw_mode[..] {
+            "esc" => self.iter().map(|o| format!("\\0x{:02x}", o)).collect(),
+            "hex" => self.iter().map(|o| format!("{:02x}", o)).collect(),
+            "C" => {
+                let mut hex_string = String::new();
+                for (i, byte) in self.iter().enumerate() {
+                    hex_string.push_str(&format!("0x{:02x}", byte));
+                    if (i + 1) % bind_config.cols == 0 {
+                        hex_string.push_str(&'\n'.to_string());
+                    } else if i + 1 < self.len() {
+                        hex_string.push_str(&", ".to_string());
+                    }
+                }
+                hex_string
+            }
+
+            unkown => panic!("got unkown option {unkown}, should not happen"),
+        }
     }
 
+    // This function can be done in a much better way
     fn format_as_hex_pretty_print(&self, bind_config: BindConfig) -> String {
         let mut hex_string = String::new();
 
@@ -34,14 +53,13 @@ impl FormatAsHex for Vec<u8> {
             hex_string.push_str(&format!("{:02x}", byte));
             // bytes_written_in_line += 1;
 
-            if (i + 1) % max_in_one_line == 0 && line_num  < max_numer_of_lines {
+            if (i + 1) % max_in_one_line == 0 && line_num < max_numer_of_lines {
                 // hex_string.push_str(&format!("  {}", from_utf8(&self[i+1-bytes_written_in_line..=i]).unwrap())); // ascii
                 hex_string.push_str(&'\n'.to_string());
                 // bytes_written_in_line = 0;
                 line_num += 1;
                 hex_string.push_str(&format!("{:08x}: ", i + 1)); // address prepend
-            }
-            else if (i + 1) % bind_config.groups == 0 {
+            } else if (i + 1) % bind_config.groups == 0 {
                 hex_string.push_str(&' '.to_string());
             }
         }
@@ -64,16 +82,17 @@ fn help() {
     println!("Bind\n");
     println!("Usage : <filename> or '-' for reading from stdin\n");
     println!("Options :");
-    println!("  -c, --cols    number of cols. [Default 8]");
-    println!("  -g, --group   number of octets per group. [Default 2]");
-    println!("  -f, --file    file to read from ('-' for stdin)");
-    // println!("  -l, --len     stop after <len> octets");
-    // println!("  -a            prepend byte");
+    println!("  -c, --cols               number of cols                      [Default 8]");
+    println!("  -g, --group              number of octets per group          [Default 2]");
+    println!("  -f, --file  <file_path>  file to read from ('-' for stdin)");
+    println!("  -r, --raw   <raw_style>  choose between hex, esc and C.");
+    println!("      hex : straight hex values ie 'cafe'");
+    println!("      esc : escaped hex values ie '\\xca\\xfe'");
+    println!("      C   : C include style ie '0xca, 0xfe'");
     process::exit(0);
 }
 
-
-fn parse_int(arg: &str, next_arg : Option<String>) -> usize {
+fn parse_int(arg: &str, next_arg: Option<String>) -> usize {
     if let Some(some_arg) = next_arg {
         match some_arg.parse::<usize>() {
             Ok(o) => o,
@@ -84,11 +103,21 @@ fn parse_int(arg: &str, next_arg : Option<String>) -> usize {
     }
 }
 
-fn parse_args() ->  Result<BindConfig>  {
+fn parse_str(arg: &str, next_arg: Option<String>) -> String {
+    if let Some(some_arg) = next_arg {
+        some_arg
+    } else {
+        panic!("'{arg}' expects an argument none was given, use -h to see help");
+    }
+}
+
+fn parse_args() -> Result<BindConfig> {
     let mut hex_dump_config = BindConfig {
         cols: 8,
         groups: 2,
         file_name: "-".to_string(),
+        raw: false,
+        raw_mode: "hex".to_string(),
     };
 
     let mut args = env::args().skip(1);
@@ -103,7 +132,15 @@ fn parse_args() ->  Result<BindConfig>  {
                 } else {
                     panic!("'{arg}' expects an argument none was given, use -h to see help");
                 }
-            },
+            }
+            "-r" | "--raw" => {
+                hex_dump_config.raw = true;
+
+                hex_dump_config.raw_mode = parse_str(&arg, args.next());
+                if !matches!(hex_dump_config.raw_mode.as_str(), "hex" | "esc" | "C") {
+                    panic!("'{arg}' an argument 'hex' or 'esc', use -h to see help");
+                }
+            }
             "-c" | "--cols" => hex_dump_config.cols = parse_int(&arg, args.next()),
             "-g" | "--groups" => hex_dump_config.groups = parse_int(&arg, args.next()),
             _ => {
@@ -117,7 +154,12 @@ fn parse_args() ->  Result<BindConfig>  {
 fn main() -> Result<()> {
     let bind_config = parse_args()?;
     let buf = read_bytes(&bind_config.file_name)?;
-    let formated_buf = buf.format_as_hex_pretty_print(bind_config);
+    let formated_buf;
+    if bind_config.raw {
+        formated_buf = buf.format_as_raw_hex(bind_config);
+    } else {
+        formated_buf = buf.format_as_hex_pretty_print(bind_config);
+    }
 
     println!("{formated_buf}");
     Ok(())
